4b831190ede770823962b1f498acbb28c6a9e826
Ulises Hernandez Mendoza <42427081+uahm93@users.noreply.github.com>
2019-10-10 17:50:23 -0500

    Script para recuperar archivos XML

--- RecupeararXML.php
+++ RecupeararXML.php
@@ -1,50 +1,50 @@
 <?php
-			$conexion = mysqli_connect("localhost","uahm93","******", "XMLhistoricos", 3707) or die("Error en la conexión a MySql");
+$conexion = mysqli_connect("localhost","uahm93","******", "XMLhistoricos", 3707) or die("Error en la conexión a MySql");
 
-			require_once 'php/ext/PHPExcel-1.7.7/Classes/PHPExcel/IOFactory.php'; //Importa Librerias para leer excel
-			$objPHPExcel = PHPExcel_IOFactory::load('vendor/indici.xlsx'); //Carga excel donde estan los Id de los archvivos que necesitamos recuperar
-			$objHoja=$objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
-            $empresa = "Archivos"; //Carpeta donde se guardaran los archvios recuperados
-			
-			foreach ($objHoja as $iIndice=>$objCelda){
-				
-			    $uuid = trim($objCelda['A']);			
-				
-			    echo "=============================INICIO PROCESO===========================================\n";
-			    echo "\n". $consultar="SELECT * FROM tabla where id = '$id'"; 
-			   
-				$result = mysqli_query($conexion,$consultar);
-				if(mysqli_num_rows($result)>0){
+require_once 'php/ext/PHPExcel-1.7.7/Classes/PHPExcel/IOFactory.php'; //Importa Librerias para leer excel
+$objPHPExcel = PHPExcel_IOFactory::load('vendor/indici.xlsx'); //Carga excel donde estan los Id de los archvivos que necesitamos recuperar
+$objHoja=$objPHPExcel->getActiveSheet()->toArray(null,true,true,true);
+$empresa = "Archivos"; //Carpeta donde se guardaran los archvios recuperados
 
-				  $fila = mysqli_fetch_array($result);
-                  $nombre_archivo = $fila['uuid'];
-                  
-                  echo "\n".$ruta = "/mnt/".$fila['xml'];
-                  $url_xml = $ruta;
-                  $existsxml = is_file($url_xml);
-                  $fr_xml = 0;
-                  
-                  
-                        if($existsxml){
-                        $fichero = $url_xml; 
-                        
-	                        echo "\n".$nuevo_fichero = '/var/www/html/RecuperarXMLTimbrado/'.$empresa.'/'.$nombre_archivo.'.xml';     	
-	                        $fr_xml = copy($fichero, $nuevo_fichero);
-	                        
-	                        echo "\n".$nombre_archivo;
-							echo "\nHECHO\n";
+foreach ($objHoja as $iIndice=>$objCelda){
 
-						}else{
-						
-							echo "\n".$nombre_archivo;
-							echo "\nPROCESO: FALLIDO\n";
-						
-						}
+    $uuid = trim($objCelda['A']);			
 
-				}          
-				
- 				echo "=============================FIN PROCESO==============================================\n";
+    echo "=============================INICIO PROCESO===========================================\n";
+    echo "\n". $consultar="SELECT * FROM tabla where id = '$id'"; 
+
+	$result = mysqli_query($conexion,$consultar);
+	if(mysqli_num_rows($result)>0){
+
+	  $fila = mysqli_fetch_array($result);
+$nombre_archivo = $fila['uuid'];
+
+echo "\n".$ruta = "/mnt/".$fila['xml'];
+$url_xml = $ruta;
+$existsxml = is_file($url_xml);
+$fr_xml = 0;
+
+
+if($existsxml){
+$fichero = $url_xml; 
+
+	echo "\n".$nuevo_fichero = '/var/www/html/RecuperarXMLTimbrado/'.$empresa.'/'.$nombre_archivo.'.xml';     	
+	$fr_xml = copy($fichero, $nuevo_fichero);
+
+	echo "\n".$nombre_archivo;
+				echo "\nHECHO\n";
+
+			}else{
+
+				echo "\n".$nombre_archivo;
+				echo "\nPROCESO: FALLIDO\n";
+
 			}
+
+	}          
+
+	echo "=============================FIN PROCESO==============================================\n";
+}
 ?>
 
 
